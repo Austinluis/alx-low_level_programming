8881c140a160ec123ae9794bc0ad8e47de9d90b5
Austinluis <ogunsanyalouis@gmail.com>
2023-03-06 13:45:24 -0800

    Louis

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 
-/* main - prints the letters of the alphabets on a line */
+/**
+ * main - prints the letters of the alphabets on a line
+ * Return: 0
+ */
 
 int main(void)
 {
 	char al = 'a';
 
-	do
-	{
+	do{
 		putchar(al++);
 	}
-
 	while (al <= 'z');
 
 	putchar('\n');

c9b41d91db7c96ae783f8dbb6a9d560d5d84b202
Austinluis <ogunsanyalouis@gmail.com>
2023-03-09 03:21:35 -0800

    Louis

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints all single numbers in base 10 starting from 0
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+		printf("%d", i);
+	
+	putchar('\n');
+
+	return (0);
+}

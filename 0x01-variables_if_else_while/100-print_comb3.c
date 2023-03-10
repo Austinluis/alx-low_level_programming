5d8288e823f5ae495dcf8450039590bd11316178
Austinluis <ogunsanyalouis@gmail.com>
2023-03-10 12:13:48 -0800

    Louis

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible different combinations of two digits
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i, k;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (k = i + 1;  k <= 9; k++)
+		{
+			if (i != k)
+			{
+				putchar(i + '0');
+				putchar(k + '0');
+			}
+			if (i == 8 && k == 9)
+			continue;
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}

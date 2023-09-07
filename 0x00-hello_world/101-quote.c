21c837cb5b04a0270d96cc9a9cb60758fe8bb2b2
Austinluis <ogunsanyalouis@gmail.com>
2023-09-07 23:35:11 +0100

    corrected return value

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,12 +2,12 @@
 
 /**
  * main - prints a string
- * Return: 0
+ * Return: 1
  */
 
 int main(void)
 {
 	fprintf(stderr, "and that piece of art is useful\" - Dora ");
 	fprintf(stderr, "Korpar, 2015-10-19\n");
-	return (0);
+	return (1);
 }

ea0ba7952ddab2ab798ccf865ce4c4cdf7aeca3f
Austinluis <ogunsanyalouis@gmail.com>
2024-03-25 22:56:34 +0100

    betty style

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * more_numbers -  prints 10 times the numbers, from 0 to 14, followed by a new line
+ * more_numbers -  prints 0 to 14 ten times, followed by a new line
  * Return: void
  */
 

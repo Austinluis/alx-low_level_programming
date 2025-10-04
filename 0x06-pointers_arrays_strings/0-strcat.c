4438a1b323c96acac423793dca48e5aae68842ae
Austinluis <ogunsanyalouis@gmail.com>
2025-10-04 16:42:32 +0100

    String cat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strcat - function to concatenate two strings.
+ * @dest: first string, also string to append second string to.
+ * @src: second string, also string to append to first string.
+ *
+ * Return: pointer to the appednded string.
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int len1, len2;
+
+	len1 = len2 = 0;
+
+	while (dest[len1] != '\0')
+		len1++;
+
+	while (src[len2] != '\0')
+	{
+		dest[len1] = src[len2];
+		len1++;
+		len2++;
+	}
+
+	dest[len1] = '\0';
+
+	return (dest);
+}

ff9523e2bd1bf141a6a95d70d7bada0ab390d6ad
Austinluis <ogunsanyalouis@gmail.com>
2025-10-05 22:06:39 +0100

    print integers

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer to stdout.
+ * @n: integer to print.
+ *
+ * Return: void.
+ */
+
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n *= -1;
+	}
+
+	if ((n / 10) == 0)
+	{
+		_putchar((n % 10) + '0');
+		return;
+	}
+
+	print_number(n / 10);
+	_putchar((n % 10) + '0');
+}

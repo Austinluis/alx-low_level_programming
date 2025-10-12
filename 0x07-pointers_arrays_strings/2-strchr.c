9b0f73d2780e76f090f4fccd89dd164298d5efb9
Austinluis <ogunsanyalouis@gmail.com>
2025-10-12 02:01:36 +0100

    includes search for '\0'

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -13,9 +13,9 @@ char *_strchr(char *s, char c)
 {
 	unsigned int i;
 
-	for (i = 0; s[i] != '\0'; i++)
+	for (i = 0; ; i++)
 	{
-		if (s[i] == c)
+		if (s[i] == c || s[i] == '\0')
 			return (&s[i]);
 	}
 

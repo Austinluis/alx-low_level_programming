af62aa5f204683f5c7a9baa65ecda0a934675cbe
Austinluis <ogunsanyalouis@gmail.com>
2025-10-30 17:29:13 +0100

    included the source code of used self_functions

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,6 +1,53 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include "main.h"
+
+/**
+ * _strlen - function to return the lenght of a string
+ * @s: pointer to the string
+ * Return: lenght of string
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+		len++;
+
+	return (len);
+}
+
+/**
+ * _atoi - converts a string to an integer
+ * @s: pointer to string
+ * Return: converted integer
+ */
+
+int _atoi(char *s)
+{
+	int i, n = 1, len = _strlen(s);
+	unsigned int j = 0;
+
+	for (i = 0; i < len; i++)
+	{
+		if (s[i] == '-')
+			n *= -1;
+
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			j = (j * 10) + (s[i] - '0');
+			if (!(s[i + 1] >= '0' && s[i + 1] <= '9'))
+			{
+				break;
+			}
+		}
+	}
+
+	j *= n;
+
+	return (j);
+}
+
 
 /**
  * main - prints the multiplication of two numbers

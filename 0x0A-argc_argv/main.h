ede9c5eb662225e5692c63707f04bf7f09a629c7
Austinluis <ogunsanyalouis@gmail.com>
2025-10-30 17:24:31 +0100

    header

---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,25 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+char *_strcpy(char *dest, char *src);
+int _abs(int n);
+char *_strstr(char *haystack, char *needle);
+char *_strpbrk(char *s, char *accept);
+int _isalpha(int c);
+unsigned int _strspn(char *s, char *accept);
+void _puts(char *str);
+int _islower(int c);
+char *_strncpy(char *dest, char *src, int n);
+int _strlen(char *s);
+char *_strchr(char *s, char c);
+char *_strncat(char *dest, char *src, int n);
+int _isdigit(int c);
+int _atoi(char *s);
+char *_strcat(char *dest, char *src);
+char *_memset(char *s, char b, unsigned int n);
+int _isupper(int c);
+int _strcmp(char *s1, char *s2);
+char *_memcpy(char *dest, char *src, unsigned int n);
+
+#endif /* MAIN_H */

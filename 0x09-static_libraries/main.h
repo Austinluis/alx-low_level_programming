147436eefeb2122f14ef138d2c219b29c9ecd250
Austinluis <ogunsanyalouis@gmail.com>
2025-10-30 02:22:16 +0100

    icluded prototypes for _memcpy and _strcmp

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -19,5 +19,7 @@ int _atoi(char *s);
 char *_strcat(char *dest, char *src);
 char *_memset(char *s, char b, unsigned int n);
 int _isupper(int c);
+int _strcmp(char *s1, char *s2);
+char *_memcpy(char *dest, char *src, unsigned int n);
 
 #endif /* MAIN_H */

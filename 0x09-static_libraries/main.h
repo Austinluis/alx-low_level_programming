d08dfa5529f2340a4bec596909e43bbe5bb3a9cb
Austinluis <ogunsanyalouis@gmail.com>
2025-10-30 01:56:55 +0100

    header file

---
+++ 0x09-static_libraries/main.h
@@ -0,0 +1,23 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+char *_strcpy(char *dest, char *src);
+int _abs(int n);
+char *_strstr(char *haystack, char *needle);
+char *_strpbrk(char *s, char *accept);
+int _isalpha(int c);
+unsigned int _strspn(char *s, char *accept);
+void _puts(char *str);
+int _islower(int c);
+char *_strncpy(char *dest, char *src, int n);
+int _strlen(char *s);
+char *_strchr(char *s, char c);
+char *_strncat(char *dest, char *src, int n);
+int _isdigit(int c);
+int _atoi(char *s);
+char *_strcat(char *dest, char *src);
+char *_memset(char *s, char b, unsigned int n);
+int _isupper(int c);
+
+#endif /* MAIN_H */

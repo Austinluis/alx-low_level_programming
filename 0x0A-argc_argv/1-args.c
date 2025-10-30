6a993abca0c451a1dfa57d303f019930a936649e
Austinluis <ogunsanyalouis@gmail.com>
2025-10-30 13:49:41 +0100

    number of arguments passed

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the number of arguments passed into it
+ * @argc: argument count
+ * @argv: array of pointers to passed arguments
+ *
+ * Return: EXIT_SUCCESS
+ */
+
+int main(int argc, char *argv[])
+{
+	/* Avoid unused warnings for argv */
+	(void)argv;
+
+	printf("%d\n", argc - 1);
+	exit(EXIT_SUCCESS);
+}

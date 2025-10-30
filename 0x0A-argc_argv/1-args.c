57f663dbcf200732d9ab293879fe7ab45673da17
Austinluis <ogunsanyalouis@gmail.com>
2025-10-30 17:18:52 +0100

    checking

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 /**
  * main - prints the number of arguments passed into it
  * @argc: argument count
  * @argv: array of pointers to passed arguments
  *
- * Return: EXIT_SUCCESS
+ * Return: 0 success
  */
 
 int main(int argc, char *argv[])
@@ -15,5 +14,5 @@ int main(int argc, char *argv[])
 	(void)argv;
 
 	printf("%d\n", argc);
-	exit(EXIT_SUCCESS);
+	return (0);
 }
